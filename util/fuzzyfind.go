efccdd1df2e48f011c1a2d95656af71adfa33c9b
EbrietanPhasmid <zawadiak.mrjz@gmail.com>
2025-10-10 22:07:28 +0200

    Modified db/models. Implemented basic fuzzy string comparison.

--- util/fuzzyfind.go
+++ util/fuzzyfind.go
@@ -1,3 +1,13 @@
 package util
-import(
 
+import (
+	"github.com/agnivade/levenshtein"
+)
+
+func sufficientlySimilar(s1 string, s2 string) bool {
+	distance := levenshtein.ComputeDistance(s1, s2)
+	if distance < max(len(s1), len(s2))/2 {
+		return true
+	}
+	return false
+}
